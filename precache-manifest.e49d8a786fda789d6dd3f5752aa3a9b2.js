1ccc2c3218e405352abe5dd625bdb2bb81a2a7e0
Daniel Kambich <dkambich@purdue.edu>
2020-04-18 00:29:52 -0500

    Updates

---
+++ precache-manifest.e49d8a786fda789d6dd3f5752aa3a9b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f52d096c490656ce4542e90964863f48",
+    "url": "/MealWheel/index.html"
+  },
+  {
+    "revision": "329656b020d8600129a8",
+    "url": "/MealWheel/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3c8e2c7406f023a98942",
+    "url": "/MealWheel/static/js/2.e91e5e80.chunk.js"
+  },
+  {
+    "revision": "505a6f6bdacd226f93a956d35104ccd6",
+    "url": "/MealWheel/static/js/2.e91e5e80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "329656b020d8600129a8",
+    "url": "/MealWheel/static/js/main.c3acc3a8.chunk.js"
+  },
+  {
+    "revision": "0f20fc2ed4f0e4c053c5",
+    "url": "/MealWheel/static/js/runtime-main.5a06f648.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MealWheel/static/media/logo.5d5d9eef.svg"
+  }
+]);
