1aa0e82c9f7fd137ad777614518312d8f2433d4e
Daniel Kambich <dkambich@purdue.edu>
2020-04-18 00:40:34 -0500

    Clean out uncessary files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import Winwheel from "./libraries/winwheel";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Wheel></Wheel>
     </div>
   );
+}
+
+class Wheel extends Component {
+  state = { wheel: null };
+  componentDidMount() {
+    this.setState({
+      wheel: new Winwheel({
+        canvasId: "myCanvas",
+        numSegments: 4,
+        segments: [
+          { fillStyle: "#eae56f", text: "One" },
+          { fillStyle: "#89f26e", text: "Two" },
+          { fillStyle: "#7de6ef", text: "Three" },
+          { fillStyle: "#e7706f", text: "Four" },
+        ],
+        animation: {
+          type: "spinToStop",
+          duration: 4,
+          spins: 1.5,
+          callbackFinished: this.alertWheel,
+        },
+      }),
+    });
+  }
+
+  alertWheel = (test) => {
+    console.log(test);
+    alert("You have selected " + test.text);
+  };
+
+  spinWheel = () => {
+    this.state.wheel.animation.spins = 8 + 1 * Math.random();
+    this.state.wheel.startAnimation();
+  };
+
+  resetWheel = () => {
+    this.state.wheel.stopAnimation(false); // Stop the animation, false as param so does not call callback function.
+    this.state.wheel.rotationAngle = 0; // Re-set the wheel angle to 0 degrees.
+    this.state.wheel.draw();
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <button onClick={this.spinWheel}>Spin Me</button>
+        <button onClick={this.resetWheel}>Reset</button>
+        <canvas id="myCanvas" width={250} height={250} />
+      </Fragment>
+    );
+  }
 }
 
 export default App;
