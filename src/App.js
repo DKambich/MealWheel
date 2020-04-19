caa676057e7331350b194f789616a96167c74e12
Daniel Kambich <dkambich@purdue.edu>
2020-04-18 23:32:05 -0500

    Setup React-Router and routing scheme

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
 import { Home } from "./pages/Home";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import DineIn from "./pages/DineIn";
+import CarryOut from "./pages/CarryOut";
 
 function App() {
-  return <Home></Home>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/MealWheel/home" component={Home} />
+        <Route path="/MealWheel/dinein" component={DineIn} />
+        <Route path="/MealWheel/carryout" component={CarryOut} />
+        <Redirect from="/" to="/MealWheel/home" />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,26 @@
 import React from "react";
-import { Navbar as BSNavbar, Nav } from "react-bootstrap";
+import { Navbar as BSNavbar, Nav, NavbarBrand } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import NavbarToggle from "react-bootstrap/NavbarToggle";
+import NavbarCollapse from "react-bootstrap/NavbarCollapse";
 
 export default function Navbar() {
   return (
     <BSNavbar bg="primary" variant="dark" expand="lg">
-      <BSNavbar.Brand href="#home">MealWheel</BSNavbar.Brand>
-      <BSNavbar.Toggle aria-controls="basic-navbar-nav" />
-      <BSNavbar.Collapse id="basic-navbar-nav">
+      <LinkContainer to="home">
+        <NavbarBrand>MealWheel</NavbarBrand>
+      </LinkContainer>
+      <NavbarToggle aria-controls="basic-navbar-nav" />
+      <NavbarCollapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
+          <LinkContainer to="dinein">
+            <Nav.Link>Dine In</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="carryout">
+            <Nav.Link>Carry Out</Nav.Link>
+          </LinkContainer>
         </Nav>
-      </BSNavbar.Collapse>
+      </NavbarCollapse>
     </BSNavbar>
   );
 }
