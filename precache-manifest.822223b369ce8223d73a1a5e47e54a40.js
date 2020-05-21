f429eef16a4a6038a6393e0d20836f270c90ca54
Daniel Kambich <dkambich@purdue.edu>
2020-05-20 21:12:22 -0500

    Updates

---
+++ precache-manifest.822223b369ce8223d73a1a5e47e54a40.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b45f2de06c1f762a9ab3e2e25143c1e",
+    "url": "/MealWheel/index.html"
+  },
+  {
+    "revision": "b7a59412b0ee658084fd",
+    "url": "/MealWheel/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "b7a59412b0ee658084fd",
+    "url": "/MealWheel/static/js/2.a5e1fc17.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/MealWheel/static/js/2.a5e1fc17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8db7d248405e1dd7e354",
+    "url": "/MealWheel/static/js/main.27f97dd9.chunk.js"
+  },
+  {
+    "revision": "0f20fc2ed4f0e4c053c5",
+    "url": "/MealWheel/static/js/runtime-main.5a06f648.js"
+  },
+  {
+    "revision": "97e08dab17aabcff7ce472aec09ca633",
+    "url": "/MealWheel/static/media/arnis_512.97e08dab.png"
+  },
+  {
+    "revision": "9be4f57e96a9dddd83478b39b4d8e2c2",
+    "url": "/MealWheel/static/media/azzip_pizza_512.9be4f57e.png"
+  },
+  {
+    "revision": "80386a956c9d54bdf06e6db50f802e35",
+    "url": "/MealWheel/static/media/bru_burger_512.80386a95.png"
+  },
+  {
+    "revision": "ccfbed9e1e2b1fbf8ef7d61bca4a2559",
+    "url": "/MealWheel/static/media/buffalo_wild_wings_512.ccfbed9e.png"
+  },
+  {
+    "revision": "6fc5840dc10e88bc82551f54418f94a9",
+    "url": "/MealWheel/static/media/chick_fil_a_512.6fc5840d.png"
+  },
+  {
+    "revision": "cbf6259017f1783e544f773d1dfc7e15",
+    "url": "/MealWheel/static/media/chipotle_512.cbf62590.png"
+  },
+  {
+    "revision": "5593318063113cac5acb5d748c7b02a2",
+    "url": "/MealWheel/static/media/digby's_512.55933180.png"
+  },
+  {
+    "revision": "5593318063113cac5acb5d748c7b02a2",
+    "url": "/MealWheel/static/media/digbys_512.55933180.png"
+  },
+  {
+    "revision": "85ce18234d7d1528a7077e89587ecebb",
+    "url": "/MealWheel/static/media/dog_n_suds_512.85ce1823.png"
+  },
+  {
+    "revision": "26e396ace087ca9ea8092ea5c0aa7317",
+    "url": "/MealWheel/static/media/dos_amigos_512.26e396ac.png"
+  },
+  {
+    "revision": "4a68b0a162d3aa8db8c51724bbc1a080",
+    "url": "/MealWheel/static/media/fiesta_mexican_grill_512.4a68b0a1.png"
+  },
+  {
+    "revision": "3400eedddd710435d123d12ae39d18b7",
+    "url": "/MealWheel/static/media/five_guys_512.3400eedd.png"
+  },
+  {
+    "revision": "ec6695fe0ca7d3d105583e1226ec7bce",
+    "url": "/MealWheel/static/media/happy_china_512.ec6695fe.png"
+  },
+  {
+    "revision": "98ac49d3dc22a4c21bc57283cf33b650",
+    "url": "/MealWheel/static/media/la_hacienda_512.98ac49d3.png"
+  },
+  {
+    "revision": "891e3934df10b781601f851538e5ce8c",
+    "url": "/MealWheel/static/media/nine_irish_brothers_512.891e3934.png"
+  },
+  {
+    "revision": "612da0ff70ac355c505cde489b11ad1b",
+    "url": "/MealWheel/static/media/noodles_&_company_512.612da0ff.png"
+  },
+  {
+    "revision": "49337b40b6d11996e276deb9a3e88f4e",
+    "url": "/MealWheel/static/media/qdoba_512.49337b40.png"
+  },
+  {
+    "revision": "dda1028478402e7c2b24f3c25562a0c4",
+    "url": "/MealWheel/static/media/south_street_smokehouse_512.dda10284.png"
+  },
+  {
+    "revision": "0b4e69ad260641ae5f06312bd967a2cc",
+    "url": "/MealWheel/static/media/steak_n_shake_512.0b4e69ad.png"
+  },
+  {
+    "revision": "2b4b245c8ede00314ff5839b2e0faad5",
+    "url": "/MealWheel/static/media/triple_xxx_512.2b4b245c.png"
+  },
+  {
+    "revision": "94cfe3d980f769740a83bef1a987cc1d",
+    "url": "/MealWheel/static/media/wendys_512.94cfe3d9.png"
+  }
+]);
